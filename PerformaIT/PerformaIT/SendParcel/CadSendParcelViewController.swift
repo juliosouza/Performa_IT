568e5741f9ea2f23e702b731ab9d87819e0e7454
Julio Cezar Souza <julio.souza@gmail.com>
2021-05-27 13:23:54 -0300

    Tela de cadastro

--- PerformaIT/PerformaIT/SendParcel/CadSendParcelViewController.swift
+++ PerformaIT/PerformaIT/SendParcel/CadSendParcelViewController.swift
@@ -6,16 +6,38 @@
 //
 
 import UIKit
+import CoreData
 
 class CadSendParcelViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var telefoneTextField: UITextField!
+    @IBOutlet weak var enderecoTextField: UITextField!
+    
+    var contexto:NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    
-    
-
+    @IBAction func finalizarButton(_ sender: Any) {
+        let encomenda = Encomenda(context: contexto)
+        encomenda.nome = nameTextField.text
+        encomenda.email = emailTextField.text
+        encomenda.telefone = telefoneTextField.text
+        encomenda.endereco = enderecoTextField.text
+        
+        do {
+            try contexto.save()
+            print("sucesso")
+        } catch {
+            print("erro")
+        }
+    }
     /*
     // MARK: - Navigation
 
